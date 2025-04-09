20ecbb831a112881f2361ff4b52d5073d5a3a92e
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2025-04-10 00:00:59 +0530

    discovery server added

---
+++ discovery-server/src/test/java/com/sdp/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdp.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
