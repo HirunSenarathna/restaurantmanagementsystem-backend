bb26fbe2446b6b0ebd927a26dd606c97fff13652
Hirun Senarathna <hirunsenarathna@users.noreply.github.com>
2025-05-21 14:45:37 +0530

    did some modifications

--- services/user-service/src/main/java/com/sdp/userservice/controller/EmployeeController.java
+++ services/user-service/src/main/java/com/sdp/userservice/controller/EmployeeController.java
@@ -21,37 +21,37 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @PostMapping("/register")
-    @PreAuthorize("hasRole('OWNER')")
+//    @PreAuthorize("hasRole('OWNER')")
     public ResponseEntity<EmployeeResponse> registerEmployee(@Valid @RequestBody EmployeeRegistrationRequest registrationRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.registerEmployee(registrationRequest));
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('OWNER') or hasRole('WAITER') or hasRole('CASHIER') or authentication.principal.username == @employeeService.getEmployeeById(#id).username")
+//    @PreAuthorize("hasRole('OWNER') or hasRole('WAITER') or hasRole('CASHIER') or authentication.principal.username == @employeeService.getEmployeeById(#id).username")
     public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable Long id) {
         return ResponseEntity.ok(employeeService.getEmployeeById(id));
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('OWNER')")
+//    @PreAuthorize("hasRole('OWNER')")
     public ResponseEntity<List<EmployeeResponse>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
     @GetMapping("/role/{role}")
-    @PreAuthorize("hasRole('OWNER')")
+//    @PreAuthorize("hasRole('OWNER')")
     public ResponseEntity<List<EmployeeResponse>> getEmployeesByRole(@PathVariable Role role) {
         return ResponseEntity.ok(employeeService.getEmployeesByRole(role));
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('OWNER') or hasRole('WAITER') or hasRole('CASHIER') or authentication.principal.username == @employeeService.getEmployeeById(#id).username")
+//    @PreAuthorize("hasRole('OWNER') or hasRole('WAITER') or hasRole('CASHIER') or authentication.principal.username == @employeeService.getEmployeeById(#id).username")
     public ResponseEntity<EmployeeResponse> updateEmployee(@PathVariable Long id, @Valid @RequestBody EmployeeRegistrationRequest updateRequest) {
         return ResponseEntity.ok(employeeService.updateEmployee(id, updateRequest));
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('OWNER') or hasRole('WAITER') or hasRole('CASHIER') ")
+//    @PreAuthorize("hasRole('OWNER') or hasRole('WAITER') or hasRole('CASHIER') ")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
